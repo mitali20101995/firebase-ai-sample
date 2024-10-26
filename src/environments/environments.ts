83680f42a92d3b42d58e0dc5b07eb2841d82dc15
mitaliahir3 <mitaliahir3@gmail.com>
2024-10-26 17:46:17 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyCSwxT7-7SVeVL6arC1y4thSjhrX-tPmYU',
+      authDomain: 'sample-firebase-ai-app-f696f.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-f696f',
+      storageBucket: 'sample-firebase-ai-app-f696f.appspot.com',
+      messagingSenderId: '477558807538',
+      appId: '1:477558807538:web:dee762adc469e657dc296b',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyBgd2pQn4W2k3UCb32yEJWvhoJj8ofBrdY',
+  debug_token: ''
+};
